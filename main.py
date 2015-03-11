82dcc0e0c3a311b0ad806fc391bb46085f8bb595
David Pereira <inexhero@gmail.com>
2015-03-11 01:36:50 -0500

    core project

---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os
+import pygtk
+import yaml
+import sys
+pygtk.require('2.0')
+import gtk
+
+class Table():
+
+    def __init__(self):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("destroy", lambda w: gtk.main_quit())
+        self.window.set_size_request(320, 240)
+        self.window.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("black"))
+        self.table =  gtk.Table(4, 4, homogeneous = False)
+        self.window.add(self.table)
+    def show(self):
+        self.window.show_all()
+
+def rcPath():
+    return os.path.dirname(__file__)
+
+
+def main():
+    path = rcPath()
+    path = path + "/raspiclock.yaml"
+    stream = file(path, "r")
+    try:
+        data = yaml.load(stream)
+    except ValueError:
+        sys.exit("Config file error")
+
+    t = Table()
+
+    for e in data['modules']:
+        m = data['modules'][e]
+        f = m['file']
+        name = "modules." + f
+        try:
+            mod = __import__(name, fromlist=[''])
+            comp = mod.Component()
+            t.table.attach(comp.getL(), m['x0'], m['x1'], m['y0'], m['y1'],xoptions = gtk.FILL, yoptions = gtk.FILL)
+            gtk.timeout_add(int(m['update']), comp.update)
+        except ValueError:
+            sys.exit( name + "Module Error")
+
+    t.show()
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
---
+++ modules/clock.py
@@ -0,0 +1,38 @@
+import pygtk
+import pango
+import gtk
+import time
+
+
+class Component:
+    def __init__(self, ):
+
+        f = gtk.Fixed()
+        self.L = gtk.Frame(label=None)
+        self.time = gtk.Label()
+        self.day = gtk.Label()
+        self.month = gtk.Label()
+
+        self.time.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#FFFFFF"))
+        self.time.modify_font(pango.FontDescription("GE Inspira 48"))
+
+        self.day.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#FFA500"))
+        self.day.modify_font(pango.FontDescription("GE Inspira 20"))
+
+        self.month.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#FFA500"))
+        self.month.modify_font(pango.FontDescription("GE Inspira 20"))
+
+        f.put(self.time, 10, 10)
+        f.put(self.day, 160, 10)
+        f.put(self.month, 160, 40)
+        self.L.add(f)
+
+
+    def update(self):
+        self.time.set_text(time.strftime('%H:%M'))
+        self.day.set_text(time.strftime('%d'))
+        self.month.set_text(time.strftime('%B'))
+        return True
+
+    def getL(self):
+        return self.L
